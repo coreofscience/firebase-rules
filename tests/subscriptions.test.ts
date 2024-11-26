b2693399db552ff44d2f0bed7557e94f9e27a5ce
Oscar Arbelaez <oscar@arbelaez.dev>
2024-11-26 17:06:26 -0500

    Require email verification before they buy a subscription

--- tests/subscriptions.test.ts
+++ tests/subscriptions.test.ts
@@ -17,8 +17,15 @@ describe("subscriptions", async () => {
     await assertFails(setDoc(doc(db, "subscriptions", "otherUserId"), {}));
   });
 
-  it("can be written by authenticated users", async () => {
+  it("cannot be written without an email verified", async () => {
     const db = testEnv.authenticatedContext("userId").firestore();
+    await assertFails(setDoc(doc(db, "subscriptions", "userId"), {}));
+  });
+
+  it("can be written by authenticated users with an email verified", async () => {
+    const db = testEnv
+      .authenticatedContext("userId", { email_verified: true })
+      .firestore();
     await assertSucceeds(setDoc(doc(db, "subscriptions", "userId"), {}));
   });
 });
